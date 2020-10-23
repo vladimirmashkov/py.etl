64f74967fd2564e8df7b6cd14bc42a06bae6092c
Vladimir Mashkov <vladimir.mashkov@maximumtest.ru>
2020-10-23 09:52:22 +0300

    s2_video_learningVideo

--- templates/start.py
+++ templates/start.py
@@ -1,29 +1,29 @@
-from pg2pg import sql as pg2pg_sql
-from pg2pg import etl as pg2pg_etl
-# import pg2pg as pg2pg
+from s2_video_learningVideo import sql as s2_video_learningVideo_sql
+from s2_video_learningVideo import etl as s2_video_learningVideo_etl
+# import s2_video_learningVideo as s2_video_learningVideo
 
 def main():
-    pg2pg_sql.chk_schema_stage()
-    pg2pg_sql.chk_schema_store()
-    pg2pg_sql.crt_ss()
-    pg2pg_etl.to_stage()
+    s2_video_learningVideo_sql.chk_schema_stage()
+    s2_video_learningVideo_sql.chk_schema_store()
+    s2_video_learningVideo_sql.crt_ss()
+    s2_video_learningVideo_etl.to_stage()
     
-    pg2pg_sql.crt_temp()
-    pg2pg_sql.trf_temp()
-    pg2pg_sql.chk_sd()
+    s2_video_learningVideo_sql.crt_temp()
+    s2_video_learningVideo_sql.trf_temp()
+    s2_video_learningVideo_sql.chk_sd()
 
-    pg2pg_sql.crt_temp_sd()
-    pg2pg_sql.trf_temp_sd()
+    s2_video_learningVideo_sql.crt_temp_sd()
+    s2_video_learningVideo_sql.trf_temp_sd()
 
-    pg2pg_sql.crt_temp_store()
-    pg2pg_sql.trf_temp_store()
+    s2_video_learningVideo_sql.crt_temp_store()
+    s2_video_learningVideo_sql.trf_temp_store()
     
-    pg2pg_etl.to_dwh()
-    pg2pg_sql.trf_temp_store_dwh()
-    pg2pg_sql.chk_store()
-    pg2pg_sql.trf_store()
-    pg2pg_sql.trn_clean_store()
-    pg2pg_sql.trn_clean_stage()
+    s2_video_learningVideo_etl.to_dwh()
+    s2_video_learningVideo_sql.trf_temp_store_dwh()
+    s2_video_learningVideo_sql.chk_store()
+    s2_video_learningVideo_sql.trf_store()
+    s2_video_learningVideo_sql.trn_clean_store()
+    s2_video_learningVideo_sql.trn_clean_stage()
     
     pass
 
